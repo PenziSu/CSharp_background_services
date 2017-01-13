1d4d83ae28e9a0eb88c87bda25aeb2703eddbcc8
mark <mark@ccgh.com.tw>
2017-01-13 17:03:59 +0800

    建立背景服務練習。參考：http://j796160836.pixnet.net/blog/post/40306051-%5B.net---c%23%5D-%E6%92%B0%E5%AF%AB%E5%9C%A8%E8%83%8C%E6%99%AF%E5%9F%B7%E8%A1%8C%E7%9A%84%E6%9C%8D%E5%8B%99-(windows-service)

---
+++ WindowsService1/WindowsService1/Service1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer MyTimer;
+        public Service1()
+        {
+            InitializeComponent();
+            this.AutoLog = false;
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("MySource", "MyLog");
+            }
+            eventLog1.Source = "MySource";
+        }        
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Start Say Hello Timer");
+            MyTimer = new Timer();
+            MyTimer.Elapsed += new ElapsedEventHandler(MyTimer_Elapsed);
+            MyTimer.Interval = 10 * 1000;
+            MyTimer.Start();
+        }
+        private void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            eventLog1.WriteEntry("Say Hello Timer Ticked.");
+            System.Windows.Forms.MessageBox.Show("Hello Dude");
+        }
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Stop Say Hello Timer.");
+            MyTimer.Stop();
+            MyTimer = null;
+        }
+    }
+}
