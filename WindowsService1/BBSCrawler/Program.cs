2da8a2b262971851af1b0ac8ae017e08cebc429a
mark <mark@ccgh.com.tw>
2017-01-24 14:48:55 +0800

    可以爬到登入頁面，現在要塞帳號密碼並取得sessionID。

---
+++ WindowsService1/BBSCrawler/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace BBSCrawler
+{
+    class Program
+    {                      
+        static void Main(string[] args)
+        {
+            //string uri = "http://web3.ccgh/ccghiso/";
+            string uri = "http://web.ccgh/";
+            var pageSource = string.Empty;                 
+            CookieContainer CookiesPad = new CookieContainer();            
+            HttpWebRequest rqt = (HttpWebRequest)WebRequest.Create(uri);
+            rqt.Timeout = 5000;
+            rqt.Accept = "*/*";            
+            rqt.ServicePoint.Expect100Continue = false;
+            rqt.ContentType = "application/x-www-form-urlencoded";
+            rqt.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            //rqt.Method = "POST";
+            rqt.KeepAlive = true;                       
+            rqt.AllowAutoRedirect = true;
+            //rqt.AllowWriteStreamBuffering = false;
+            rqt.ServicePoint.UseNagleAlgorithm = false;
+            rqt.CookieContainer = CookiesPad;
+            rqt.ServicePoint.ConnectionLimit = int.MaxValue;
+            var rspos = (HttpWebResponse)rqt.GetResponse();
+            foreach ( Cookie cookie in rspos.Cookies)
+            {
+                CookiesPad.Add(cookie);
+            }
+            Stream stream = rspos.GetResponseStream();
+            Encoding encode = Encoding.GetEncoding("big5");
+            StreamReader streamReader = new StreamReader(stream, encode);
+            pageSource = streamReader.ReadToEnd();
+            
+
+
+            Console.WriteLine(pageSource);
+            Console.WriteLine("STOP!");
+            Console.ReadLine();                        
+        }       
+    }
+}
--- WindowsService1/TelegramSample/MainProgram.cs
+++ WindowsService1/TelegramSample/MainProgram.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Configuration;
-using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Threading.Tasks;
-using NetTelegramBotApi;
-using NetTelegramBotApi.Requests;
-using NetTelegramBotApi.Types;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Configuration;
+using System.Net;
+using System.Threading.Tasks;
 using TelegramSample.Models;
 
 namespace TelegramSample
@@ -25,14 +24,20 @@ namespace TelegramSample
             var cityUrl = "http://hotels.ctrip.com/citylist";
             var cityList = new List<City>();
             var cityCrawler = new SimpleCrawler();
+
+            //OnStart
             cityCrawler.OnStart += (s, e) =>
             {
                 Console.WriteLine("爬蟲開始抓取地址:" + e.Uri.ToString());
             };
+
+            //OnError
             cityCrawler.OnError += (s, e) =>
             {
                 Console.WriteLine("爬蟲抓取出現錯誤:" + e.Uri.ToString() + "，異常狀態:" + e.Exception.Message);
             };
+
+            //OnCompleted
             cityCrawler.OnCompleted += (s, e) =>
             {              
                 var links = Regex.Matches(e.PageSource, @"<a[^>]+href=""*(?<href>/hotel/[^>\s]+)""\s*[^>]*>(?<text>(?!.*img).*?)</a>", RegexOptions.IgnoreCase);
@@ -41,8 +46,7 @@ namespace TelegramSample
                     var city = new City
                     {
                         CityName = match.Groups["text"].Value,
-                        Uri = new Uri("http://hotels.ctrip.com" + match.Groups["href"].Value
-                    )
+                        Uri = new Uri("http://hotels.ctrip.com" + match.Groups["href"].Value)                    
                     };
                     if (!cityList.Contains(city)) cityList.Add(city);
                     Console.WriteLine(city.CityName + "|" + city.Uri);
@@ -52,7 +56,9 @@ namespace TelegramSample
                 Console.WriteLine("耗時：" + e.Milliseconds + "毫秒");
                 Console.WriteLine("線程：" + e.ThreadId);
                 Console.WriteLine("地址：" + e.Uri.ToString());
+                Console.ReadLine();
             };
+
             cityCrawler.Start(new Uri(cityUrl)).Wait();
         }
     }
