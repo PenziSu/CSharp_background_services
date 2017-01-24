788c793246ad26ae1b3df6c3284f2eb2ce5e6dbd
mark <mark@ccgh.com.tw>
2017-01-24 17:16:39 +0800

    可以提交帳號密碼並登入，目前卡在轉址畫面以及餅乾盒內沒有餅乾。 參考資料：http://jjnnykimo.pixnet.net/blog/post/25277457-c%23,-%E4%BD%BF%E7%94%A8-httpwebrequest-%E5%90%91%E7%B6%B2%E7%AB%99%E6%8F%90%E4%BA%A4%E8%B3%87%E6%96%99

--- WindowsService1/BBSCrawler/Program.cs
+++ WindowsService1/BBSCrawler/Program.cs
@@ -13,36 +13,67 @@ namespace BBSCrawler
         static void Main(string[] args)
         {
             //string uri = "http://web3.ccgh/ccghiso/";
+            //建立網址
             string uri = "http://web.ccgh/";
-            var pageSource = string.Empty;                 
-            CookieContainer CookiesPad = new CookieContainer();            
-            HttpWebRequest rqt = (HttpWebRequest)WebRequest.Create(uri);
-            rqt.Timeout = 5000;
-            rqt.Accept = "*/*";            
-            rqt.ServicePoint.Expect100Continue = false;
-            rqt.ContentType = "application/x-www-form-urlencoded";
-            rqt.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
-            //rqt.Method = "POST";
-            rqt.KeepAlive = true;                       
-            rqt.AllowAutoRedirect = true;
-            //rqt.AllowWriteStreamBuffering = false;
-            rqt.ServicePoint.UseNagleAlgorithm = false;
-            rqt.CookieContainer = CookiesPad;
-            rqt.ServicePoint.ConnectionLimit = int.MaxValue;
-            var rspos = (HttpWebResponse)rqt.GetResponse();
-            foreach ( Cookie cookie in rspos.Cookies)
+            //初始畫內容
+            var pageSource = string.Empty;
+            //建立Big5編碼
+            Encoding big5Code = Encoding.GetEncoding("big5");
+            //建立登入資料
+            string data = "T1=13432&T2=1qa2ws3ed&B1=登入";
+            //將登入資料轉換編碼變成payload
+            byte[] payload = Encoding.ASCII.GetBytes(data);
+            //建立Cookie容器
+            CookieContainer CookiesPad = new CookieContainer();                        
+            //建立Hppt請求資訊
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(uri); //設定網址
+            req.Timeout = 5000; //設定逾時設定5000
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"; //設定標題
+            req.ServicePoint.Expect100Continue = false; 
+            req.ContentType = "application/x-www-form-urlencoded"; //設定內容型態
+            req.ContentLength = payload.Length; //設定內容長度
+            req.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"; //模擬瀏覽器
+            req.KeepAlive = true; 
+            req.Referer = "http://web.ccgh/"; //設定參考網址
+            req.AllowAutoRedirect = true; //設定重新導向
+            //req.AllowWriteStreamBuffering = false;
+            req.ServicePoint.UseNagleAlgorithm = false; //設定演算法Nagle
+            req.CookieContainer = CookiesPad; //設定Cookie
+            req.ServicePoint.ConnectionLimit = int.MaxValue; //設定最大連線數
+            req.Method = "POST";  //設定提交方法
+
+            //提交包含資料的請求
+            using (Stream reqStream = req.GetRequestStream())
+            {
+                reqStream.Write(payload, 0, payload.Length);
+            }
+
+            //取得回應
+            var rspos = (HttpWebResponse)req.GetResponse();
+
+            //以迴圈將cookie資訊寫入Cookie儲存器
+            foreach (Cookie cookie in rspos.Cookies)
             {
                 CookiesPad.Add(cookie);
+                Console.WriteLine("=======");
+                Console.WriteLine(cookie);
+                Console.WriteLine("=======");
             }
+
+            //將回應轉成Stream
             Stream stream = rspos.GetResponseStream();
-            Encoding encode = Encoding.GetEncoding("big5");
-            StreamReader streamReader = new StreamReader(stream, encode);
+
+            //將串流重新編碼後放入串流讀取器
+            StreamReader streamReader = new StreamReader(stream, big5Code);
+
+            //將串流資訊填入字串變數
             pageSource = streamReader.ReadToEnd();
-            
 
-
+            //呈現回應內容
             Console.WriteLine(pageSource);
-            Console.WriteLine("STOP!");
+
+            //顯示STOP文字當做結尾
+            Console.WriteLine("STOP!");            
             Console.ReadLine();                        
         }       
     }
